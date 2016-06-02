3d9ff0400b77bcd00c999c5735963a8496c5c75f
Ignacio Ballesteros <ignacioballesterosgonzalez@gmail.com>
2016-06-03 00:58:13 +0200

    File exists check.

--- Calle30/scrapper.py
+++ Calle30/scrapper.py
@@ -3,17 +3,17 @@ import requests
 import json
 import csv
 import time
-from time import gmtime, strftime
+import os
 
+# url de a la que se hace la peticion GET
 url = "http://www.mc30.es/images/xml/DatosTrafico.xml"
 
+# Headers necesarios para que la peticion GET este autorizada
 headers = {'headersHost': 'www.mc30.es', 'User-Agent': 'Python script', 'Accept': ' text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8', 'Accept-Language': 'en-US,en;q=0.5', 'Accept-Encoding': 'gzip, deflate', 'Connection': 'keep-alive'}
 
 fieldnames = ['totalVehiculosTunel', 'totalVehiculosTunelCalle30',
               'velocidadmediaTunel', 'velocidadMediaSuperficie',
               'fecha']
-
-last_time_request = ""
 
 
 def get_new_values():
@@ -29,29 +29,39 @@ def get_new_values():
     return values
 
 
-# start_time = strftime("%Y-%m-%d %H:%M:%S", gmtime())
 name = "datasetTraficoCalle30"
 extension = ".csv"
 path = "./data/"
+file_path = path + name + extension
 
 
-with open(path + name + extension, 'w+') as csvfile:
-    header = csvfile.readline()
+if (not os.path.exists(file_path)):
+    open(file_path, 'w')
+
+
+with open(file_path, 'r+') as csvfile:
+    header = csvfile.readline().strip().split(',')
     if (header != fieldnames):
-            spamwrite = csv.writer(csvfile, delimiter=' ',
-                                   quotechar='|', quoting=csv.QUOTE_MINIMAL)
-            writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-            writer.writeheader()
+        # spamwrite = csv.writer(csvfile, delimiter=' ',
+        #                        quotechar='|', quoting=csv.QUOTE_MINIMAL)
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+        writer.writeheader()
+        print("Nuevo fichero creado")
+        last_time_request = ""
+    else:
+        last = csvfile.readlines()[-1]
+        last_time_request = last.strip().split(',')[-1]
 
 
-with open(path + name + extension, 'a') as csvfile:
+with open(file_path, 'a') as csvfile:
     while True:
         values = get_new_values()
+        # Comprobacion de que los datos no se encuentran ya en el fichero
         if (str(last_time_request) != values['fecha']):
             # Peticiones periodicas de los datos
             last_time_request = values['fecha']
-            spamwrite = csv.writer(csvfile, delimiter=' ',
-                                   quotechar='|', quoting=csv.QUOTE_MINIMAL)
+            # spamwrite = csv.writer(csvfile, delimiter=' ',
+            #                        quotechar='|', quoting=csv.QUOTE_MINIMAL)
             writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
             writer.writerow(values)
             print(values)
